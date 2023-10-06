d930c03f5971c8ee251934467edfaad769a41382
Cindy Wu <ciindy.wu@gmail.com>
2023-10-05 15:48:51 -1000

    x-vercel-signature

--- app/api/pause/route.ts
+++ app/api/pause/route.ts
@@ -1,5 +1,20 @@
+import { createHmac } from "crypto";
+
 export async function POST(request: Request) {
+  // Get the signature from headers
+  const signature = request.headers.get("x-vercel-signature");
+  // Get the raw body
   const body = await request.json();
+
+  // Create a hash using your VERCEL_WEBHOOK_SECRET
+  const hashedBody = createHmac("sha256", process.env.VERCEL_WEBHOOK_SECRET)
+    .update(body)
+    .digest("hex");
+
+  // Compare the hash with the signature
+  if (hashedBody !== signature) {
+    return new Response("Invalid signature", { status: 403 });
+  }
 
   const teamId = body.teamId;
   await fetch(
