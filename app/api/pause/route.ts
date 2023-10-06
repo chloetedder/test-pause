8389e7cc8bfb78e816c0ca3c743a95a933b06eb3
Cindy Wu <ciindy.wu@gmail.com>
2023-10-05 15:21:47 -1000

    add /pause route that accepts webhook

---
+++ app/api/pause/route.ts
@@ -0,0 +1,16 @@
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const teamId = body.teamId;
+  await fetch(
+    `https://api.vercel.com/v1/projects/prj_suxaOaYmfhRQAdK5E9wGVzYm9kqn/pause?teamId=${teamId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    }
+  );
+  return new Response("Project paused");
+}
